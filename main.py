7510e69094815f3ea54d72ba2fa4f54ea5460825
atmichael7 <50531800+atmichael7@users.noreply.github.com>
2023-09-26 16:06:14 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -35,133 +35,99 @@ root.config(bg="lightgray")
 1 mi = 5280 ft
 1 mi = 1760 yd
 1 mi = 1 mi
-'''
+
 oneCm = [1, 0.393701, 0.0328084, 0.0108361, 0.0000062137]
 oneIn = [2.54, 1, 0.08333, 0.0277778, 0.000015783]
 oneFt = [30.48, 12, 1, 0.333333, 0.000189394]
 oneYd = [91.44, 36, 3, 1, 0.000568182]
 oneMi = [160934, 63360, 5280, 1760, 1]
+'''
+# Data for the top left portion (Imperial - Distance)
+topLeftData = [0.0,0.0,0.0,0.0,0.0]
 
+
+# IMPERIAL UNIT CONVERSIONS ====================================================================================
 units = ["centimeters", "inches", "feet", "yards", "miles"]
 rates = ["second", "minute", "hour"]
+
+# Dictionary to associate the matrix [x][] position in the matrix unit conversion array
+N_units = ["cm", "in", "ft", "yd", "mi"]
+# Dictionary to associate in/out rates with their respective values 
+N_rates = [("sec", 1), ("min", 60), ("hr", 3600)]
+# Dictionary to associate output rate with respective values
+N_outRates = [("Per second", 1), ("Per minute", 60), ("Per hour", 3600)]
 
 #             cm       in         ft          yd          mi
 allUnits =  [[1,       0.393701,  0.0328084,  0.0108361,  0.0000062137], # cm
              [2.54,    1,         0.08333,    0.0277778,  0.000015783],  # in
              [30.48,   12,        1,          0.333333,   0.000189394],  # ft
              [91.44,   36,        3,          1,          0.000568182],  # yd
-             [160934,  63360,     5280,       1760,       1]]            # mi 
+             [160934,  63360,     5280,       1760,       1]]            # mi
 
-# WILL NEED TO BE UPDATED AFTER VALUES ARE CHANGED
-topLeftData = [0.0,0.0,0.0,0.0,0.0]
-
-def getInputValue():
-    return tLeftInput.get()
-
-def getInputUnit():
-    return clicked1.get()
-
-def getInputRate():
-    return clicked2.get()
-
-def getOutputRate():
-    return clicked3.get()
-
-#def updateData(data):
-#    topLeftDisplay(data)
-
-# Upper left Imperial Speed
-def testUpdateData(data):
-    sampleList = [20.0, 15.0, 10.0, 5.0, 1.0]
-    for i in range(10):
-        for x in range(len(data)):
-            sampleList[x-1] = sampleList[x-1]*2
-            data[x-1] = sampleList[x-1]
-    topLeftDisplay(data)
-
-# value is the input value, x is the 
-def tL_FinCalc(val, inUnit, inRate):
-    if inRate == 1 or inRate == 4 or inRate == 7 or inRate == 10:
-        if inUnit == 0:
-            pass
-        elif inUnit == 1:
-            pass
-        elif inUnit == 2:
-            pass
-        elif inUnit == 3:
-            pass
-        elif inUnit == 4:
-            pass
-        elif inUnit == 5:
-            pass
+# END IMPERIAL UNIT CONVERSIONS ====================================================================================           
 
 
-
-# Dictionary to associate the matrix [x][] position in the matrix unit conversion array
-N_units = ["cm", "in", "ft", "yd", "mi"]
-
-# Dictionary to associate in/out rates with their respective values 
-N_rates = [("sec", 1), ("min", 60), ("hr", 3600)]
-
-# Dictionary to associate output rate with respective values
-N_outRates = [("Per second", 1), ("Per minute", 60), ("Per hour", 3600)]
-
-# More efficient code
+# Perform calculations for the top left section
 def N_topLeftCalculate():
-    if tLeftInput.get() == "":
+    if tLeftInput.get() == "": # see if input is just empty spaces, ifSo: do nothing and send no errors 
             return False
-    
     try:
-        inputValue = float(tLeftInput.get())
-    except:
-        tempVariable = tLeftInput.get()
-        for i in range(1,30):
-            if tempVariable == " "*i:
-                return False
-        tLeftInput.insert(0, "Invalid -> ")
-        return False
+        inputValue = float(tLeftInput.get()) # see if we can convert the input to float
+    except:                                  # ifNot -> except (Means string was entered and can't convert to float value)
+        tempVariable = tLeftInput.get() # temp variable of the input value     
+        for i in range(1,30):           # loop through the multiple concatinations to see if multiple empty spaces are entered
+            if tempVariable == " "*i:   # ifSo, no big deal and send no error message
+                return False            
+        tLeftInput.insert(0, "Invalid -> ") # otherwise and invalid string has been entered into the field
+        return False                        # thus show error on input field and do no calculations
+    # the reason for not caring about empty strings before or after numeric input is that the program can still do calculations with empty spaces before or after
 
-    inputValue = float(tLeftInput.get())
-    inputUnit = clicked1.get()
+    inputValue = float(tLeftInput.get()) 
+    inputUnit = clicked1.get()  
     inputRate = clicked2.get()
     outputRate = clicked3.get()
     matrix = 0
 
     # set units to the corresponding unit matrix
-    for i in range(len(N_units)):
-        if N_units[i] == inputUnit:
-            matrix = i
-            break
+    for i in range(len(N_units)): # go thru each index of N_units array
+        if N_units[i] == inputUnit: # see if the current index in array is equal to the current input unit string
+            matrix = i  # ifSo, set matrix value equal to the current index of i since they are 1.1 linked
+            break # done, end loop
 
     # set input rate
-    for i in range(len(N_rates)):
-        if N_rates[i][0] == inputRate:
-            inputRate = N_rates[i][1]
-            break
+    for i in range(len(N_rates)): # go thru the N_rates array
+        if N_rates[i][0] == inputRate: # see if the current index at position ZERO is equal to the inputted rate string 
+            inputRate = N_rates[i][1] # ifSo, set inputRate = the current index in N_rates at position ONE
+            break # done, end loop
+    # had to use matrix style list since we have a string associated to a value
+    # [i][0] is the 1.1 where the string will match to seek what is inputted
+    # [i][1] is the associated value to what was inputted
 
     # set output rate
-    for i in range(len(N_rates)):
-        if N_outRates[i][0] == outputRate:
-            outputRate = N_outRates[i][1]
-            break
+    for i in range(len(N_rates)): # go thru N_rates
+        if N_outRates[i][0] == outputRate: # see if the current index at position ZERO == the inputted outputRate from user
+            outputRate = N_outRates[i][1] # ifSo, set outputRate = the current index of list at position 1 
+            break # done
+    # [i][0] is the 1.1 where the string will match to seek what is inputted
+    # [i][1] is the associated value to what was inputted
 
     # perform calculation
-    for i in range(len(topLeftData)):
+    for i in range(len(topLeftData)): # go through each index in the topLeftData list
         topLeftData[i] = (inputValue / inputRate) * allUnits[matrix][i] * outputRate
+        # at current index in data list, set it equal to the inputted int value divided by its rate (1 / 60 / 3600 --> sec / min / hour) respectively
+        #   then multiply that by the allUnits matrix at the set MATRIX at position i
+        #       the way the allUnits is essentially a table that has all the conversions for a desired unit in each index with respect to an input unit
+        #   lastly multiply by the outputRate (1 / 60 / 3600 --> sec / min / hour) respectively
 
     # display the data
     topLeftDisplay(topLeftData)
         
-       
+# called by the clear button to clear the top left output fields
 def topLeftClear():
-    for currIndex in range(len(topLeftData)):
-        topLeftData[currIndex] = 0
-        tLeftInput.delete(0, END)
-    topLeftDisplay(topLeftData)
-
-def notifyDisplay():
-    N_topLeftCalculate()
-    topLeftDisplay(topLeftData)
+    for currIndex in range(len(topLeftData)): # go through each index in the data list
+        topLeftData[currIndex] = 0  # set current index = 0
+        tLeftInput.delete(0, END) # clears the integer entry field 
+    topLeftDisplay(topLeftData) # display the cleaned output fields (the entry will update itself from above .delete method)
 
 # Top left output displays Imperial - Speed
 def topLeftDisplay(data):
@@ -233,7 +199,7 @@ clicked3.set("Per second")
 
 i = 4
 for text, selected in options3:
-    Radiobutton(tLeftLabel, text = text, variable = clicked3, value = selected, command = notifyDisplay,bg = "lightgray").grid(row = i, column = 0, padx = 5, sticky = "w")
+    Radiobutton(tLeftLabel, text = text, variable = clicked3, value = selected, command = N_topLeftCalculate,bg = "lightgray").grid(row = i, column = 0, padx = 5, sticky = "w")
     i += 1
 
 # Output fields
